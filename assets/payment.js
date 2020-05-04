d8cd0f30af785bf95908cad0c65e0cb1297f5caf
Arturo Garcia <arturogs.me@gmail.com>
2020-05-03 22:54:16 -0500

    other commit

--- assets/payment.js
+++ assets/payment.js
@@ -1,17 +1,21 @@
-$('#the-button').on('click', function() {
-  $.ajax({
-    url: window.location.host + 'checkout.php',
-    type: 'POST',
-    dataType: 'json',
-    success: function(json) {
-      if(json['status'] == 'success') {
-        window.location = json['redirect'];
-      } else {
-        alert('No logramos conectar con Mercado Pago');
+$(document).ready(function() {
+  console.log('button pressed');
+  $('#the-button').on('click', function() {
+    $.ajax({
+      url: window.location.host + 'checkout.php',
+      type: 'POST',
+      dataType: 'json',
+      success: function(json) {
+        console.log('response: ' + json);
+        if(json['status'] == 'success') {
+          window.location = json['redirect'];
+        } else {
+          alert('No logramos conectar con Mercado Pago');
+        }
+      },
+      error: function(xhr, ajaxOptions, thrownError) {
+        alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
       }
-    },
-    error: function(xhr, ajaxOptions, thrownError) {
-      alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
-    }
+    });
   });
 });
