8a3c003d2fd56ae8eb1925b1b34088491a384649
Arturo Garcia <arturogs.me@gmail.com>
2020-05-03 23:21:13 -0500

    other commit

--- success.php
+++ success.php
@@ -7,6 +7,10 @@
     <div class="container">
       <p class="tite">Recibimos tu pago con éxito</p>
       <p class="message">En breve estaremos enviando tus productos :)</p>
+      <p>Payment method id:<?php echo $_GET['payment_method_id'] ?></p>
+      <p>Monto pagado:<?php echo $_GET['transaction_amount'] ?></p>
+      <p>Orden del pedido:<?php echo $_GET['external_reference'] ?></p>
+      <p>ID Mercado Pago:<?php echo $_GET['id'] ?></p>      
     </div>
   </body>
 </html>
